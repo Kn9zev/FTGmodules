d58b8a0191e17e814e9ae6cda4e793f62ab4d3a1
Kn9zev <vova.kn9zev@gmail.com>
2023-01-10 23:02:25 +0200

    Update vidal.py

--- vidal.py
+++ vidal.py
@@ -12,7 +12,7 @@ class VidalMod(loader.Module):
     """Vidal Поиск препарата"""
     strings = {"name": "VidalMod"}
 
-    async def wcmd(self, m):
+    async def vicmd(self, m):
         """.vi <препарат>"""
         lek = utils.get_args_raw(m).replace(" ", "%20")
         r = requests.get("https://www.vidal.ru/search?q=" + lek + "&bad=on")
