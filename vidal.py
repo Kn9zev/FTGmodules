e7ac9d4ead331981412aa6c5094090b2d70abbd5
Kn9zev <vova.kn9zev@gmail.com>
2023-01-11 00:06:43 +0200

    Update vidal.py

--- vidal.py
+++ vidal.py
@@ -15,8 +15,6 @@ class VidalMod(loader.Module):
     async def vicmd(self, m):
         """.vi <препарат>"""
         lek = utils.get_args_raw(m).replace(" ", "%20")
-        r = requests.get("https://knyazev.xyz/vidal/?lek=" + lek, headers={
-"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
-        r.close()
+        r = requests.get("https://knyazev.xyz/vidal/?lek=" + lek)
 
         await utils.answer(m, r.text)
