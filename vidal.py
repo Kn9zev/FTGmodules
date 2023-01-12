3968d0ae1d9f557ad25926eb514ef3a13c2e5894
Kn9zev <vova.kn9zev@gmail.com>
2023-01-12 23:06:23 +0200

    Update vidal.py

--- vidal.py
+++ vidal.py
@@ -9,13 +9,13 @@ from .. import loader, utils
 
 @loader.tds
 class VidalMod(loader.Module):
-    """Поиск препарата и его описания по справочнику Видаля (vidal.ru) v0.1a / by Vova Knyazev"""
+    """Поиск препарата и его описания по справочнику Видаля (vidal.ru) / by Knyazev"""
     strings = {"name": "VidalInfo"}
 
     async def vicmd(self, m):
         """.vi <препарат>"""
         lek = utils.get_args_raw(m).replace(" ", "%20")
-        user_agent = {'User-agent': 'Mozilla/5.0'}
-        r = requests.get("https://q-design.ru/vidal/?lek=" + lek, headers=user_agent)
+        user_agent = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+        r = requests.get("https://knyazev.xyz/vidal/?lek=" + lek, headers=user_agent)
 
         await utils.answer(m, r.text)
